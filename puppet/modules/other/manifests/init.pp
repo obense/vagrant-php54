990faf73a4b61fdc5e16df7d22aef43cd446c193
Ph3nol <ph3@slynett.com>
2013-01-01 23:24:03 +0100

    Stéphane Py Vim config added (not fully OK with BundleInstall for instant)

--- puppet/modules/other/manifests/init.pp
+++ puppet/modules/other/manifests/init.pp
@@ -2,6 +2,7 @@ class other
 {
     $packages = [
         "curl", 
+        "git",
         "vim"
     ]
     
@@ -10,5 +11,12 @@ class other
         $packages:
             ensure  => latest,
             require => Exec['apt-get update']
+    }
+
+    exec
+    {
+        'vim-config':
+            command => 'git clone https://github.com/stephpy/vim-config.git /home/vagrant/.vim-config && ln -s /home/vagrant/.vim-config/.vim /home/vagrant/.vim && ln -s /home/vagrant/.vim-config/.vimrc /home/vagrant/.vimrc && mkdir /home/vagrant/.vim/bundle && git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle && sudo chown -R vagrant:vagrant /home/vagrant/.vim* && vim +BundleInstall +qa',
+            require => Package['git']
     }
 }
